1b73a2dadc4df7b65595325c5b2c80b1a9e31c20
Seokhee Park <seokhee_park@nexcubecorp.com>
2025-12-18 19:20:29 +0900

    feat: 모바일 별 효과 추가

--- src/components/ChristmasTree.tsx
+++ src/components/ChristmasTree.tsx
@@ -15,18 +15,20 @@ const ChristmasTree: React.FC<ChristmasTreeProps> = ({
   return (
     <div className="tree-wrapper">
       <div className="tree-container">
-        {/* 트리 꼭대기 별 */}
-        <svg
-          className="tree-star-svg"
-          viewBox="0 0 50 50"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <polygon
-            className="tree-star"
-            points="25,2 29,18 46,18 32,28 37,45 25,35 13,45 18,28 4,18 21,18"
-            fill="#FFD700"
-          />
-        </svg>
+        {/* 트리 꼭대기 별 - Safari 버그 우회용 wrapper */}
+        <div className="tree-star-wrapper">
+          <svg
+            className="tree-star-svg"
+            viewBox="0 0 50 50"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <polygon
+              className="tree-star"
+              points="25,2 29,18 46,18 32,28 37,45 25,35 13,45 18,28 4,18 21,18"
+              fill="#FFD700"
+            />
+          </svg>
+        </div>
 
         {/* 트리 이미지 */}
         <img
